a3696ac99eb95175e5c242309a44f9e592df7339
gaurav-1829 <pundirgaurav461@gmail.com>
2026-01-12 18:38:15 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,35 @@
+const n=123
+console.log(typeof n);
+
+const str="hello world"
+console.log(typeof str);
+
+const loggedin=true
+console.log(typeof loggedin);
+
+const data=null
+console.log(typeof data);
+
+const undef=undefined
+console.log(typeof undef);
+
+const bignum=1213124234234n
+console.log(typeof bignum);
+
+const sym=Symbol("123")
+console.log(typeof sym);
+
+const obj={
+    name:"gaurav",
+    age:22
+}
+console.log(typeof obj);
+
+const myfun=function(){
+    console.log("this is my function");
+}
+
+const arr=[1,2,3,4,5]
+console.log(typeof arr);
+
+console.log(typeof myfun);
---
+++ datatypeconversion.js
@@ -0,0 +1,9 @@
+let a=33
+console.log(typeof(a));
+let b=String(a)
+console.log(typeof b);
+console.log(b);
+
+let c=Boolean(b)
+console.log(typeof c);
+console.log(c);
