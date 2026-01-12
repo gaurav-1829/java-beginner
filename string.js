bdda23c1f1445222c3a2b445b33b48efbff76e65
gaurav-1829 <pundirgaurav461@gmail.com>
2026-01-12 19:40:10 +0530

    String

---
+++ string.js
@@ -0,0 +1,19 @@
+// const name="gaurav"
+// const repocount=34
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+// const name= new String("gaurav")
+// console.log(name);
+
+// console.log(name.indexOf("u"));
+
+// console.log(name.charAt(2));
+
+// console.log(name.substring(0,4));
+
+const name="gaurav pundir"
+// console.log(name.trim());
+
+// console.log(name.replace("pundir","singh"));
+
+console.log(name.split(" "));
