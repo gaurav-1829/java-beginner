63859d7112eec5af52f7f21162e5818a42914897
gaurav-1829 <pundirgaurav461@gmail.com>
2026-01-13 07:47:17 +0530

    Nums_and_Maths

---
+++ nums_maths.js
@@ -0,0 +1,49 @@
+const value=new Number(44)
+// console.log(value);
+
+// console.log(value.toString());
+
+// console.log(value.toFixed(2));
+
+// console.log(value.toPrecision(1));
+
+// console.log(value.valueOf());
+
+// console.log(value.toLocaleString(2));
+
+
+//+++++++++++++++++++++++++Maths++++++++++++++++++++++++
+
+// console.log(Math.PI);
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.round(6.7));
+
+// console.log(Math.ceil(5.1));
+
+// console.log(Math.floor(5.9));
+
+// console.log(Math.sqrt(25));
+
+// console.log(Math.max(2,4,3,6));
+
+// console.log(Math.min(2,3,4,1));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10) + 1);
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+// min=10
+// max=20
+
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+min=23
+max=35
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+console.log(Math.pow(2,4));
