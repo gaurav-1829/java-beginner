89364da3735768e630087193696e12fdecba61bc
gaurav-1829 <pundirgaurav461@gmail.com>
2026-01-13 08:18:49 +0530

    Date and Time

---
+++ date_time.js
@@ -0,0 +1,26 @@
+// let mydate=new Date();
+// console.log(mydate.toLocaleString())
+
+// console.log(mydate.toLocaleDateString())
+
+// console.log(mydate.toDateString());
+
+// console.log(typeof mydate);
+
+// let createmydate=new Date(2027,0,17,5,13);
+
+// console.log(createmydate.toDateString());
+
+// let mydate=new Date("2028-04-23")
+
+// console.log(mydate.toLocaleString())
+
+let mytime=Date.now();
+
+console.log(Math.floor(mytime/1000));
+
+let mydate=new Date();
+
+console.log(mydate.getDate());
+
+console.log(mydate.getMonth()+1);
