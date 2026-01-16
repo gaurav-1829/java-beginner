13b28453c72f15df55ba343bd8469a636d702fef
gaurav-1829 <pundirgaurav461@gmail.com>
2026-01-16 07:15:29 +0530

    Arrays

--- arrays.js
+++ arrays.js
@@ -1,4 +1,58 @@
 //Arrays
-let a=[1,2,3,4,5];
-console.log(a);
-console.log(typeof a);
+// const myarr=[0,1,2,3,4,5];
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(myarr[4]);
+
+// myarr.push(6);
+// console.log(myarr);
+
+// myarr.pop();
+
+// console.log(myarr);
+
+// myarr.unshift(9);
+// console.log(myarr);
+
+// myarr.shift();
+// console.log(myarr);
+
+// console.log("A",myarr);
+// const arr1=myarr.slice(1,3);
+// console.log(arr1);
+
+// console.log("B",myarr);
+// const arr2=myarr.splice(1,3);
+// console.log(arr2);
+
+// console.log("C",myarr);
+
+
+// const myarr=[1,2,3,4,5]
+// const myarr2=[6,7,8,9]
+
+// myarr.push(myarr2)
+// console.log(myarr);
+
+// myarr.push(...myarr2)
+// console.log(myarr);
+
+// const myarr3=myarr.concat(myarr2)
+// console.log(myarr3);
+
+// const myarr4=[...myarr,...myarr2]
+// console.log(myarr4);
+
+// const myarr=[1,2,3,[4,5,6],7,[8,9,[10,11]]]
+// console.log(myarr.flat(Infinity))
+
+// console.log(Array.from("gaurav"))
+// console.log(Array.isArray("gaurav"))
+console.log(Array.from({name:"gaurav"}))
+
+// let score1=100
+// let score2=200
+// let score3=300
+
+// console.log(Array.of(score1,score2,score3))
